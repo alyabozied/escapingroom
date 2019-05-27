f49200bd2d8671440c3d41c5dba3ac0277011806
alyabozied <42724850+alyabozied@users.noreply.github.com>
2019-05-27 02:05:57 +0200

    grabbing things

--- Source/escapingroom/Grabber.cpp
+++ Source/escapingroom/Grabber.cpp
@@ -1,15 +1,18 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "Grabber.h"
+#include "Engine/DemoNetDriver.h"
 
+#include "GameFramework/Actor.h"
+#include "Engine/World.h"
+#include "Runtime/Engine/Classes/GameFramework/PlayerController.h"
+#include "Runtime/Engine/Public/DrawDebugHelpers.h"
 // Sets default values for this component's properties
 UGrabber::UGrabber()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
 
 
@@ -19,15 +22,106 @@ void UGrabber::BeginPlay()
 	Super::BeginPlay();
 	UE_LOG(LogTemp, Warning, TEXT("Grabber report for reporting duty!"));
 	// ...
+	/// lock for attached physcis handle by going ot owner then get the phycishandle component
+	GetPhysicsHandle();
+	GetInputComponent();
+	grabingthing = false;
+}
+
+void UGrabber::GetInputComponent()
+{
+	input = GetOwner()->FindComponentByClass<UInputComponent>();
+	if (input) {
+		input->BindAction("Grab", IE_Pressed, this, &UGrabber::ActionGrab);
+		
+	}
+	else {
+		UE_LOG(LogTemp, Error, TEXT("error %s attached input handler not found"), *GetOwner()->GetName())
+	}
+}
+
+void UGrabber::GetPhysicsHandle()
+{
+	physics = GetOwner()->FindComponentByClass<UPhysicsHandleComponent>();
+	if (physics==nullptr) {
+		UE_LOG(LogTemp, Error, TEXT("error %s attached physics handler not found"), *GetOwner()->GetName())
+	}
 	
 }
 
-
+void UGrabber::ActionGrab() {
+	
+	if (!grabingthing)
+	{///get object in reach
+		Grab();
+		
+	}
+	else Release();
+	grabingthing = !grabingthing;
+}
+void UGrabber::Grab()
+{
+	auto HitResult = ObjectInReach(); auto component = HitResult.GetComponent();
+	auto actor = HitResult.GetActor();
+	///attach it ot pyhiscs handle or in a simple way to grab it
+	if (actor)
+	{		if (HitResult.GetActor())
+			physics->GrabComponent(
+				component,
+				NAME_None,
+				component->GetOwner()->GetActorLocation(),
+				true //allowrotation
+			);
+	}
+}
 // Called every frame
 void UGrabber::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	// ...
+	/// get player view point
+	location = Owner->GetActorLocation();
+	rotation = Owner->GetActorRotation();
+	///move object grabbed with the player
+	if (physics->GrabbedComponent)
+		physics->SetTargetLocation(GetplayerReach());
 }
 
+FHitResult UGrabber::ObjectInReach()
+{	
+	///get player location and reach
+	FVector PointOfViewLocation=Getplayerlocation();
+	FVector LineTraceEndPoint = GetplayerReach();
+	FCollisionQueryParams TraceParams(FName(TEXT("")), false, GetOwner());
+	///line-trace to see what is in the player reach
+	FHitResult Hit;
+	GetWorld()->LineTraceSingleByObjectType(
+		OUT Hit,
+		PointOfViewLocation,
+		LineTraceEndPoint,
+		FCollisionObjectQueryParams(ECollisionChannel::ECC_PhysicsBody),
+		TraceParams
+	);
+	return Hit;
+}
+
+void UGrabber::Release() {
+	physics->ReleaseComponent();
+}
+FVector UGrabber::Getplayerlocation() {
+	FVector PointOfViewLocation;
+	FRotator PointOfViewRotation;
+	///get player point of view
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(PointOfViewLocation, PointOfViewRotation);
+	FVector unitvectortoplayer = PointOfViewRotation.Vector();
+	return PointOfViewLocation;
+
+}
+FVector UGrabber::GetplayerReach() {
+	FVector PointOfViewLocation;
+	FRotator PointOfViewRotation;
+	///get player point of view
+	GetWorld()->GetFirstPlayerController()->GetPlayerViewPoint(PointOfViewLocation, PointOfViewRotation);
+	FVector unitvectortoplayer = PointOfViewRotation.Vector();
+	FVector LineTraceEndPoint = unitvectortoplayer * Reach + PointOfViewLocation;
+	return LineTraceEndPoint;
+}
--- Source/escapingroom/Grabber.h
+++ Source/escapingroom/Grabber.h
@@ -1,7 +1,9 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-
+#include "Public/CollisionQueryParams.h"
+#include "Components/InputComponent.h"
+#include "PhysicsEngine/PhysicsHandleComponent.h"
 #include "CoreMinimal.h"
 #include "Components/ActorComponent.h"
 #include "Grabber.generated.h"
@@ -10,19 +12,40 @@
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
 class ESCAPINGROOM_API UGrabber : public UActorComponent
 {
-	GENERATED_BODY()
-
+	GENERATED_BODY()	
+private:
+	UPROPERTY(EditAnywhere)
+		float Reach = 100.f;
+	UPhysicsHandleComponent* physics=nullptr;
+	UInputComponent*input = nullptr;
+	// recast and grab things in reach area
+	void Grab();
+	void ActionGrab();
+	void Release();
+	bool grabingthing;
+	FVector Getplayerlocation();
+	FVector GetplayerReach();
 public:	
 	// Sets default values for this component's properties
+
 	UGrabber();
 
 protected:
 	// Called when the game starts
+	AActor*Owner = GetOwner();
+	FVector location;
+	FRotator rotation;
 	virtual void BeginPlay() override;
+
+	void GetInputComponent();
+
+	void GetPhysicsHandle();
 	
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
+	FHitResult ObjectInReach();
+	
 		
 };
